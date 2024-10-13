f373b3b1778ec17818a9365d3d6f8db402eddc64
Icyyoung719 <11642834+yangege7666666@user.noreply.gitee.com>
2024-10-13 20:36:38 +0800

    修改命名

--- resources/src/func/power2poly.m
+++
@@ -1,7 +0,0 @@
-% power2poly takes a GF(2^m) matrix and a power of alpha and returns the
-% corresponding polynomial, in n-Tuple Form. GF has n-Tuple Form elements, 
-% each of which are 5 bits wide.  
-function [ poly ] = power2poly( GF, power )
-	location = power + 2;
-	poly = GF(location,:);
-end
--- resources/src/utils/TestPPGenerator.m
+++
@@ -1,8 +0,0 @@
-% 示例：生成 GF(2^3) 的 BCH 码生成多项式
-GF = gf(0:7, 3);  % 构建 GF(2^3)
-m = 3;  % 域阶
-t = 2;  % 纠错能力
-
-genPoly = GeneratorPolynomialGenerator(GF, m, t);
-disp('生成多项式：');
-disp(genPoly);
